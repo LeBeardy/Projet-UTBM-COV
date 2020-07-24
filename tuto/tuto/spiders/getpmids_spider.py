0d6105ff7a03362219914f1f1682fb10ced46145
Paul-Antoine PECHMEJA-RICHARD <parichard@bazin.fr>
2020-07-24 16:34:18 +0200

    link between databse and crawler

--- tuto/tuto/spiders/getpmids_spider.py
+++ tuto/tuto/spiders/getpmids_spider.py
@@ -7,35 +7,22 @@ import os.path
 from os import path
 import ndjson
 from  json import dumps
-from Modules.manager import Manager
-
-def createFile( path_file, data):
-    option = 'w'
-    if path.exists(path_file) :
-        option = 'a'
-
-    with open(path_file, option) as f:
-        f.write(data)
+from tuto.spiders.Modules.manager import Manager
 
 class QuotesSpider(scrapy.Spider):
     name = "getpmids"
-    start_urls = [
-        'https://www.ncbi.nlm.nih.gov/research/coronavirus-api/feed/?filters=%7B%7D',
-    ]
+    start_urls = [ 'https://www.ncbi.nlm.nih.gov/research/coronavirus-api/feed/?filters=%7B%7D', ]
 
     def parse(self, response):
         today = date.today() - timedelta(days=1)
         today = today.strftime("%d %b %Y")
         url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson"
-        path_to_data =  os.getcwd() +"\\data"
-        filename_log = '\\log_'+today+'.jl'
-        filename_data = '\\data_'+today+'.jl'
         pmids = []
 
         if not os.path.exists('data'):
             os.makedirs('data')
 
-        #database_manager = Manager('data/wikiData.db')
+        database_manager = Manager('data/data.db')
 
         for post in response.xpath('//channel/item'):
             if today in post.xpath('pubDate//text()').extract_first():
@@ -47,17 +34,4 @@ class QuotesSpider(scrapy.Spider):
         resp = rep.json(cls=ndjson.Decoder)
 
         for item in resp:
-            article ={"id": item["id"], "title": item["passages"][0]["text"], "text": item["passages"][1]["text"], "authors": item["authors"]}
-            createFile(path_to_data+filename_data, dumps(article))
-
-
-            #print(resp[0]["id"])
-            #print(resp[0]["passages"][0]["text"])
-            #print(resp[0]["passages"][1]["text"])
-            #print(resp[0]["authors"])
-
-            #print(item["passages"])
-            #database_manager.insert_articles_content(item["id"],item["title"],item["content"])
-
-        #createFile(path_to_data+filename_data, rep.text)
-        #createFile(path_to_data+filename_log, dumps(json))
+            database_manager.insert_articles_content( item["id"],item["passages"][0]["text"], item["passages"][1]["text"], dumps(item["authors"]))
