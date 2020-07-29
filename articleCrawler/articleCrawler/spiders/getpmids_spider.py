a8d7cd0cfa937247231e0d90ef5e18c3cb1d7efc
Paul-AntoinePechmeja-Richard <paul-antoine.pr@gmail.com>
2020-07-29 12:28:10 +0200

    clean up and add log(print) for the crawler

--- articleCrawler/articleCrawler/spiders/getpmids_spider.py
+++ articleCrawler/articleCrawler/spiders/getpmids_spider.py
@@ -7,7 +7,7 @@ import os
 import ndjson
 from  json import dumps
 from Modules.data.manager import Manager
-
+from json import loads
 from itertools import islice
 from itertools import zip_longest
 
@@ -29,29 +29,26 @@ class ArticlesSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        project_folder = os.getcwd()
-        today = date.today()# - timedelta(days=1)
-        today = today.strftime("%d %b %Y")
+        today = (date.today() - timedelta(days=1)).strftime("%d %b %Y")
         url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson"
         pmids = []
+        print("fetch pour le jour : " + today)
+        database_manager = Manager(os.getcwd() + '/data/database/data.db')
 
-        database_manager = Manager(project_folder + '/data/database/data.db')
-        ret = []
         for post in response.xpath('//channel/item'):
-            #if today in post.xpath('pubDate//text()').extract_first():
+            if today in post.xpath('pubDate//text()').extract_first():
                 pmids.append( post.xpath('link//text()').extract_first().split("/")[6])
 
         if pmids:
             for groupe in grouper(pmids, 1000) :
+                print("groupe de taile :" + len(groupe))
                 json ={"pmids": list(filter(None,list(groupe))) }
                 rep = requests.post(url, json=json)
                 resp = rep.json(cls=ndjson.Decoder)
 
                 for item in resp:
                     database_manager.insert_articles_content( item["id"],item["passages"][0]["text"], item["passages"][1]["text"], dumps(item["authors"]))
-                    #re.append( AticleItem (pmid= item["id"],title=item["passages"][0]["text"],content= item["passages"][1]["text"],autohrs= dumps(item["authors"])))
                     print("article %s inseré" % item["id"])
             print ("Data successfully fetched", 201)
-            return ret
         else :
             print ("No data found on : \"https://www.ncbi.nlm.nih.gov/research/pubtator/index.html?view=docsum&query=$LitCovid\"", 404)
