968137736ac9b3d445f7815a528be45c0f6a79b2
Paul-AntoinePechmeja-Richard <paul-antoine.pr@gmail.com>
2020-08-10 11:11:24 +0200

    readme

--- Modules/LDA/Cleaner.py
+++ Modules/LDA/Cleaner.py
@@ -12,73 +12,73 @@ from nltk.corpus import stopwords, wordnet
 
 class Cleaner:
 
-  def __init__(self):
-  """
-  Init function of the class Cleaner
-  """
-    # Punctuations and stopwords
-    self.punctuation = set(string.punctuation)
-    self.stoplist = set(stopwords.words('english'))
+    def __init__(self):
+        """
+        Init function of the class Cleaner
+        """
+        # Punctuations and stopwords
+        self.punctuation = set(string.punctuation)
+        self.stoplist = set(stopwords.words('english'))
 
-    # LDA
-    self.dictionary = corpora.Dictionary()
-    self.lemma = WordNetLemmatizer()
+        # LDA
+        self.dictionary = corpora.Dictionary()
+        self.lemma = WordNetLemmatizer()
 
-  def remove_punctuation(self, text):
-      """
-      Function that remove the punctuation of a text.
-      :param text:      Text to clean
-      :return:          Cleaned text
-      """
-      return ''.join([char for char in text if char not in self.punctuation])
-
-
-  def remove_numbers(self, text):
-       """
-       Function that remove the numbers of a text.
-       :param text:      Text to clean
-       :return:          Cleaned text
-       """
-      return ''.join([char for char in text if not char.isdigit()])
+    def remove_punctuation(self, text):
+        """
+        Function that remove the punctuation of a text.
+        :param text:      Text to clean
+        :return:          Cleaned text
+        """
+        return ''.join([char for char in text if char not in self.punctuation])
 
 
-  def remove_stopwords(self, text):
-       """
-       Function that remove the stop words of a text.
-       :param text:      Text to clean
-       :return:          Cleaned text
-       """
-      return ' '.join([word for word in text.split() if word not in self.stoplist])
+    def remove_numbers(self, text):
+        """
+        Function that remove the numbers of a text.
+        :param text:      Text to clean
+        :return:          Cleaned text
+        """
+        return ''.join([char for char in text if not char.isdigit()])
 
 
-  def remove_single_chars(self, text):
-       """
-       Function that remove the single characters word of a text.
-       :param text:      Text to clean
-       :return:          Cleaned text
-       """
-      return ' '.join([word for word in text.split() if len(word) > 1])
+    def remove_stopwords(self, text):
+        """
+        Function that remove the stop words of a text.
+        :param text:      Text to clean
+        :return:          Cleaned text
+        """
+        return ' '.join([word for word in text.split() if word not in self.stoplist])
 
 
-  def lemmatize(self, text):
-       """
-       Function that lemmatize the text.
-       :param text:      Text to clean
-       :return:          Cleaned text
-       """
-      return ' '.join([self.lemma.lemmatize(word) for word in text.split()])
+    def remove_single_chars(self, text):
+        """
+        Function that remove the single characters word of a text.
+        :param text:      Text to clean
+        :return:          Cleaned text
+        """
+        return ' '.join([word for word in text.split() if len(word) > 1])
 
 
-  def clean_text(self, text):
-       """
-       Function that fully clean a text.
-       :param text:      Text to clean
-       :return:          Fully cleaned text
-       """
-      text = text.replace('\n', '')
-      text = self.remove_punctuation(text)
-      text = self.remove_numbers(text)
-      text = self.remove_stopwords(text)
-      text = self.remove_single_chars(text)
-      text = self.lemmatize(text)
-      return text
+    def lemmatize(self, text):
+        """
+        Function that lemmatize the text.
+        :param text:      Text to clean
+        :return:          Cleaned text
+        """
+        return ' '.join([self.lemma.lemmatize(word) for word in text.split()])
+
+
+    def clean_text(self, text):
+        """
+        Function that fully clean a text.
+        :param text:      Text to clean
+        :return:          Fully cleaned text
+        """
+        text = text.replace('\n', '')
+        text = self.remove_punctuation(text)
+        text = self.remove_numbers(text)
+        text = self.remove_stopwords(text)
+        text = self.remove_single_chars(text)
+        text = self.lemmatize(text)
+        return text
--- README.md
+++ README.md
@@ -1,9 +1,49 @@
 # Projet-UTBM-COV
 <a href="https://codeclimate.com/github/Paul-AntoinePechmeja-Richard/Projet-UTBM-COV/maintainability"><img src="https://api.codeclimate.com/v1/badges/3ac20147cbaf04868448/maintainability" /></a>
 
+This project is a LDA-based covid article recommender system for the UTBM (Université de technologie de Belfort Montbéliard) a french university.
+This project use sqlite3 to store the data.
+This projet can be divided into 3 differents parts.
 
-https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmids=32729463
+##The crawler
 
-https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmcids=PMC7251362,PMC7392602
+To fetch the differents data about the articles, we use Scrapy to fetch the list of articles.
 
-https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids=32729463
+First we read the RSS of [LitCovid](https://www.ncbi.nlm.nih.gov/research/coronavirus/docsum) to fetch the differents pmid of the article.
+Then we ask the [PubTator](https://www.ncbi.nlm.nih.gov/research/pubtator/index.html?view=docsum&query=$LitCovid) API to get the article's data.
+
+Few exemple of the data we got :
+
+The [RSS](https://www.ncbi.nlm.nih.gov/research/coronavirus-api/feed/?filters=%7B%7D)
+The [abstact article](https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmids=32729463)
+The [full artcle](https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/biocjson?pmcids=PMC7251362,PMC7392602)
+The [conversion pmid to pmcid](https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?ids=32729463)
+
+##The LDA
+
+Once we got all the data of the articles, we generate the LDA model.
+First we clean with the Cleaner, then we generate the model with gensim, we save the ressources generated into the data/LDA folder
+The Evaluator class allows to generate the recommendations from an article's content
+
+##The API
+
+We can have access to all the previous element through an API.
+You can have acces of all the endpoints here : http://localhost:5000/api/ui/
+
+##The database
+
+We stock the article's data into the table articleData.
+
+
+|     pmid     |  pmcid     | title  |     content_abstract     |  content_full     | authors  |     date_pub     |  journal_pub     |
+| :----------: |:----------:| :-----:| :----------------------: |:-----------------:| :-------:| :--------------: |:----------------:|
+| 32729463     | PMC7251362 | title  | content                  | content           | authors  | 01/01/2020       | journal          |
+
+
+##Usage
+
+To launch the API
+  ```bash
+  python ./api.py
+  ```
+Then you can use the differents endpoints find here : http://localhost:5000/api/ui/
